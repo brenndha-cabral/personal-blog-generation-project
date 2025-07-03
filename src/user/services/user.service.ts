4047d5bc5600235cf1b050915d4fd86ee4094f84
brenndha-cabral <cabralbrenndha@gmail.com>
2025-07-03 11:16:17 -0300

    feat: create user service

---
+++ src/user/services/user.service.ts
@@ -0,0 +1,64 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Bcrypt } from '../../auth/bcript/bcript';
+import { User } from '../entities/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private bcrypt: Bcrypt,
+  ) {}
+
+  async findByUser(user: string): Promise<User | null> {
+    return await this.userRepository.findOne({
+      where: {
+        user: user,
+      },
+    });
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async findUserById(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!user)
+      throw new HttpException('Usuário não encontrado!', HttpStatus.NOT_FOUND);
+
+    return user;
+  }
+
+  async create(user: User): Promise<User> {
+    const findUser = await this.findByUser(user.user);
+
+    if (findUser)
+      throw new HttpException('O Usuário já existe!', HttpStatus.BAD_REQUEST);
+
+    user.password = await this.bcrypt.encryptPassword(user.password);
+    return await this.userRepository.save(user);
+  }
+
+  async update(user: User): Promise<User> {
+    await this.findUserById(user.id);
+
+    const findUser = await this.findByUser(user.user);
+
+    if (findUser && findUser.id !== user.id)
+      throw new HttpException(
+        'Usuário (e-mail) já cadastrado!',
+        HttpStatus.BAD_REQUEST,
+      );
+
+    user.password = await this.bcrypt.encryptPassword(user.password);
+    return await this.userRepository.save(user);
+  }
+}
