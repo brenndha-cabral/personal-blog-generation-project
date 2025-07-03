b65fc5ffd5135bdb3996aacae206b6d041882f2d
brenndha-cabral <cabralbrenndha@gmail.com>
2025-07-03 12:17:24 -0300

    feat: add files on UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { AuthModule } from '../auth/auth.module';
+import { UserService } from './services/user.service';
+import { UserController } from './controllers/user.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [],
-  controllers: [],
-  exports: [],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
+  providers: [UserService],
+  controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
