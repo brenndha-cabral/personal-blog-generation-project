24588ccc9ff79d5e848409a38d02eef28f19f85d
brenndha-cabral <cabralbrenndha@gmail.com>
2025-07-04 17:20:31 -0300

    test: create new tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,135 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Testes dos módulos User e Auth (e2e)', () => {
   let app: INestApplication<App>;
+  let token: any;
+  let userId: any;
+  let themeId: any;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          entities: [__dirname + './../src/**/entities/*.entity.ts'],
+          synchronize: true,
+          dropSchema: true, // apenas em arquivos de teste
+        }),
+        AppModule,
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('01 - Deve cadastrar um novo usuário', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/users')
+      .send({
+        name: 'Root',
+        user: 'root@root.com',
+        password: 'senhaForte',
+        photo: '--',
+      })
+      .expect(201);
+
+    userId = response.body.id;
+  });
+
+  it('02 - Não deve cadastrar um usuário duplicado', async () => {
+    await request(app.getHttpServer())
+      .post('/users')
+      .send({
+        name: 'Root',
+        user: 'root@root.com',
+        password: 'senhaForte',
+        photo: '--',
+      })
+      .expect(400);
+  });
+
+  it('03 - Deve autenticar o usuário (login)', async () => {
+    const resposta = await request(app.getHttpServer())
+      .post('/users/login')
+      .send({
+        user: 'root@root.com',
+        password: 'senhaForte',
+      })
+      .expect(200);
+
+    token = resposta.body.token;
+  });
+
+  it('04 - Deve listar todos os usuários', async () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/users')
+      .set('Authorization', `${token}`)
+      .send({})
+      .expect(200);
+  });
+
+  it('05 - Deve listar o usuário pelo id', async () => {
+    return request(app.getHttpServer())
+      .get(`/users/${userId}`)
+      .set('Authorization', `${token}`)
+      .send({})
+      .expect(200);
+  });
+
+  it('06 - Deve atualizar um usuário', async () => {
+    return request(app.getHttpServer())
+      .put('/users')
+      .set('Authorization', `${token}`)
+      .send({
+        id: userId,
+        name: 'Root Atualizado',
+        user: 'root@root.com',
+        password: 'senhaForte',
+        photo: '-',
+      })
       .expect(200)
-      .expect('Hello World!');
+      .then((response) => {
+        expect(response.body.name).toEqual(response.body.name);
+      });
+  });
+
+  it('07 - Deve cadastrar um novo tema', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/themes')
+      .set('Authorization', `${token}`)
+      .send({
+        description:
+          'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+      })
+      .expect(201);
+
+    themeId = response.body.id;
+  });
+
+  it('08 - Deve cadastrar uma nova postagem', async () => {
+    return request(app.getHttpServer())
+      .post('/posts')
+      .set('Authorization', `${token}`)
+      .send({
+        title: 'Lorem Ipsum',
+        text: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+        theme: themeId,
+        user: userId,
+      })
+      .expect(201);
   });
 });
